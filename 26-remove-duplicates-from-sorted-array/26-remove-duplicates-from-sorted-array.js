907199202635daec04c0d251abd7df6de280450d
Bikram.Rumba <76753523+BikramRumba@users.noreply.github.com>
2022-04-12 14:05:31 -0500

    Time: 68 ms (96.66%), Space: 45.2 MB (18.06%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+        let j = 0;
+    for(i=1; i<nums.length; i++){
+          if(nums[i] !==nums[j]){
+              j++;
+              nums[j]=nums[i]
+          }
+        
+     
+          
+    }
+    return j+1
+    
+};
